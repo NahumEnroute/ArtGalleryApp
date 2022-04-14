319e1247029295dffda0703b1a0fe56a19e4961e
Nahum <david.caldera@enroutesystems.com>
2022-04-14 17:33:16 -0600

    Add her custom parser

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -1,8 +1,21 @@
 require 'faraday_middleware'
 
+class CustomParser < Faraday::Response::Middleware
+  def on_complete(env)
+    json = MultiJson.load(env.body.to_json, symbolize_keys: true)
+    env[:body] = {
+      errors: json[:errors],
+      metadata: json[:metadata]
+    }
+    env[:body][:data] = json[:data][:data]
+    env[:body][json[:data][:type].to_sym] = json[:data][:data]
+    env
+  end
+end
+
 Her::API.setup url: ENV['API_HOST'], request: { timeout: 10, open_timeout: 2 } do |c|
   # Request
-  c.use Faraday::Request::UrlEncoded
+  c.use CustomParser
 
   # Response
   c.use Her::Middleware::DefaultParseJSON
